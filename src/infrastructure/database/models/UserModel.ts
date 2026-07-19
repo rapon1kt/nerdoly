837e1facca5c3b921ce4cbbf445b31e4f74951de
rapon1kt <shinnatensei@gmail.com>
2026-07-19 00:22:28 -0300

    feat: creating initial model of user

---
+++ src/infrastructure/database/models/UserModel.ts
@@ -0,0 +1,28 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = models.User || model("User", UserSchema);
