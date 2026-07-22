ce7c1b246fd6c5807c4d82f10b7d4ae715310c56
kaio <kaitozhiroki@gmail.com>
2026-07-21 22:50:37 -0300

    feat: creating nav bar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton";
+
+export default function NavBar() {
+  return (
+    <div className="flex justify-center font-baloo pt-18">
+      <nav className="w-dvh flex justify-between items-center">
+        <div className="">
+          <span className="font-extrabold text-3xl">
+            nerdoly<span className="text-pinkcard">.</span>
+          </span>
+        </div>
+        <div className="flex gap-4 text-white font-semibold">
+          <CustomButton text="Sobre" />
+          <CustomButton text="Entrar" />
+        </div>
+      </nav>
+    </div>
+  );
+}
