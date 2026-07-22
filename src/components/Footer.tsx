f04dbed8c50b3b0f97e430418ccaceb63992aaf0
kaio <kaitozhiroki@gmail.com>
2026-07-21 22:52:02 -0300

    feat: creating footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center items-center p-5">
+      <p className="text-[#8A8A80] text-sm">
+        nerdoly — feito por nerds, para nerds que querem passar.
+      </p>
+    </div>
+  );
+}
