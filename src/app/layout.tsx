449ef0f9c6b1dcaf6202f818f29c3f789916f1c7
kaio <kaitozhiroki@gmail.com>
2026-07-21 22:50:24 -0300

    feat: adding main text font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Baloo_2 } from "next/font/google";
+
+const baloo = Baloo_2({
+  weight: ["400", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-baloo",
+});
 
 export const metadata: Metadata = {
   title: "Nerdoly",
@@ -13,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${baloo.variable}
+        min-h-screen bg-[#FFF9E9]
+        bg-[image:linear-gradient(#E9E2CC_1px,transparent_1px),linear-gradient(90deg,#E9E2CC_1px,transparent_1px)]
+        bg-[size:28px_28px]`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
